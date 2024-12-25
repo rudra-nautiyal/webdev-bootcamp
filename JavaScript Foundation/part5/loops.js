4a8c1f2f00c4fc7801034f9b94a7eecb105ca177
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2024-12-25 17:27:47 +0530

    JS loops

---
+++ JavaScript Foundation/part5/loops.js
@@ -0,0 +1,16 @@
+// Guidelines for Loops
+// 1. Keyword 2. Termination condition 3. Body of loop
+// Types of Loops
+// 1. while 2. do/while 3. for 4. for/in 5. for/of 6. forEach
+
+/* 
+1. Write a `while` loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named `sum`.
+*/
+
+let i = 1;
+let sum = 0;
+
+while (i < 6) {
+  sum += i;
+  i++;
+}
