ed0034eef576006102d1bb5c152e435925e00729
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-16 17:40:57 +0530

    JavaScript fundamentals end

---
+++ JavaScript Foundation/part6/functions.js
@@ -0,0 +1,69 @@
+/* 
+1. Write a function named `makeTea` that takes one parameter, `typeOfTea`, and returns a string like `"Making green tea"` when called with `"green tea"`. 
+Store the result in a variable named `teaOrder`.
+*/
+
+function makeTea(typeOfTea) {
+  return `Making ${typeOfTea}`;
+}
+
+let teaOrder = makeTea("green tea");
+
+/* 
+2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
+Call `confirmOrder` from within `orderTea` and return the result.
+*/
+
+function orderTea(teaType) {
+  function confirmOrder() {
+    return `Order confirmed for chai`;
+  }
+
+  return confirmOrder(); // JS Executation Context
+}
+
+/* 
+3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
+Store the result in a variable named `totalCost`.
+*/
+
+const calculateTotal = (price, quantity) => price * quantity;
+
+let totalCost = calculateTotal(10, 20);
+
+/* 
+4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
+Return the result of calling `makeTea`.
+*/
+
+function makeTea(typeOfTea) {}
+
+function processTeaOrder(teaFunction) {
+  return teaFunction("earl grey");
+}
+
+let order = processTeaOrder(makeTea); // Higher order fn
+
+/* 
+5. Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
+Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
+*/
+
+function createTeaMaker() {
+  return function (teatype) {
+    return `Making ${teatype}`;
+  };
+}
+
+let teaMaker = createTeaMaker();
+console.log(teaMaker("green tea"));
+
+/* Closure (Impt for Interview)
+When a function is defined inside another function, it forms a closure. The inner function has access to:
+
+1. Its own scope (variables defined within it).
+
+2. The outer function's scope (variables and functions defined in the outer function).
+
+3. Global scope (variables defined outside any function).
+*/
