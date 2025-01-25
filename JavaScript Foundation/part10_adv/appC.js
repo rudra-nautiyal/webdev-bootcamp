1e8727a51a07c97bc4bddd72ef73a0a2e3620ec2
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-01-26 03:00:57 +0530

    JS Advance

---
+++ JavaScript Foundation/part10_adv/appC.js
@@ -0,0 +1,5 @@
+//  depreciated method, try to use ES6 unless specially specified.
+
+const mathOperations = require("./mathoperationC.js");
+
+console.log(mathOperations.add(2, 2));
---
+++ JavaScript Foundation/part10_adv/appM.js
@@ -0,0 +1,12 @@
+// module import file
+
+// default import
+import multiply from "./mathoperationM.js";
+
+// named import
+import {add, subtract} from "./mathoperationM.js";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
+console.log(subtract(2, 2));
+
