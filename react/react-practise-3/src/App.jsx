3632f026c7413e62489d0cc7386213a6c066d658
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-04-05 02:23:07 +0530

    react routing and layout

--- react/react-practise-3/src/App.jsx
+++ react/react-practise-3/src/App.jsx
@@ -1,7 +1,77 @@
 import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  redirect,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
-  return <div>hello, world</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout></Layout>}>
+            <Route
+              path="/neet/online-coaching-class-11"
+              element={<Class11Program />}
+            />
+            <Route
+              path="/neet/online-coaching-class-12"
+              element={<Class12Program />}
+            />
+            <Route path="/" element={<Landing />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Link to="/">Allen</Link>|
+      <Link to="/neet/online-coaching-class-11">Class 11</Link>|
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      <div style={{ height: "90vh" }}>
+        <Outlet></Outlet>
+      </div>
+      Footer
+    </div>
+  );
+}
+
+function ErrorPage() {
+  return <div>not a page, use correct url nigger.</div>;
+}
+
+function Landing() {
+  return <div>Welcome to allen</div>;
+}
+
+function Class11Program() {
+  return <div>NEET programs for Class 11th</div>;
+}
+
+function Class12Program() {
+  const navigate = useNavigate();
+
+  function redirectUser() {
+    navigate("/");
+  }
+
+  return (
+    <div>
+      NEET programs for Class 12th
+      <br />
+      <button onClick={redirectUser}>Go back to landing page</button>
+    </div>
+  );
 }
 
 export default App;
